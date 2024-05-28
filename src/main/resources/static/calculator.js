63502d978b19ab9f9c20eabc05954d748cd3cde5
JohnnyDeng6 <jzydeng@gmail.com>
2024-05-28 14:55:22 -0700

    a1

---
+++ src/main/resources/static/calculator.js
@@ -0,0 +1,87 @@
+var count = 2;
+function addRow() {
+    var table = document.getElementById("myTable");
+
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML = "Activity " + count;
+    cell2.innerHTML = "A" + count;
+    cell3.innerHTML = `
+        <form>
+            <input name="weight" type="text">
+        </form>
+    `;
+    cell4.innerHTML = `
+        <form id="form1">
+            <input name="x" type="text" oninput="updatePercent(this)">
+        </form>
+        /
+        <form id="form2">
+            <input name="n" type="text" oninput="updatePercent(this)">
+        </form>
+    `;
+    count++;
+}
+
+function meanFunc() {
+    let adTD = document.getElementsByTagName('td');
+    let len = adTD.length;
+    var mean = 0;
+    for (var i = 3; i < len; i+=5) { //i = grade cell
+        const form1Input = adTD[i].querySelector('input[name="x"]');
+        const form2Input = adTD[i].querySelector('input[name="n"]');
+
+        let x = Number(form1Input.value);
+        let n = Number(form2Input.value);
+
+        mean += x/n;
+        console.log(mean);
+    }
+    let result = mean/(len/5) * 100 + "%";
+    document.getElementsByClassName('results')[0].innerHTML = result;
+}
+
+function weightedFunc() {
+    let adTD = document.getElementsByTagName('td');
+    let len = adTD.length;
+    var mean = 0;
+    var totalWeight = 0;
+    for (var i = 3; i < len; i+=5) { //i = grade cell
+        const form1Input = adTD[i].querySelector('input[name="x"]');
+        const form2Input = adTD[i].querySelector('input[name="n"]');
+        const form3Input = adTD[i-1].querySelector('input[name="weight"]');
+
+        let x = Number(form1Input.value);
+        let n = Number(form2Input.value);
+        let w = Number(form3Input.value);
+
+        mean += x/n*w;
+        totalWeight += w;
+    }
+    let result = mean/totalWeight * 100 + "%";
+    document.getElementsByClassName('results')[0].innerHTML = result;
+
+}
+
+function updatePercent(inputElement) {
+    const cell = inputElement.closest('td');
+
+    const form1Input = cell.querySelector('input[name="x"]');
+    const form2Input = cell.querySelector('input[name="n"]');
+    const displayDiv = cell.nextElementSibling;
+
+    let x = Number(form1Input.value);
+    let n = Number(form2Input.value);
+
+    console.log(`x: ${x}`);
+    console.log(`n: ${n}`);
+    const percent = x/n * 100 +"%";
+
+    displayDiv.textContent = `${percent}`;
+}
+
+
